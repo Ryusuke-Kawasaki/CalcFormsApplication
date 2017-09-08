ff25aae3e413f639fa29942500d1bef62b290c93
kawasaki <kawasaki@i-real.info>
2017-09-08 11:30:50 +0900

    プロジェクト ファイルを追加します。

---
+++ CalcFormsApplication/Calc.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace CalcFormsApplication
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multi = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 96);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(549, 96);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "数値１";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "数値2";
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(115, 205);
+            this.plus.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(98, 74);
+            this.plus.TabIndex = 5;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(280, 205);
+            this.minus.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(98, 74);
+            this.minus.TabIndex = 6;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multi
+            // 
+            this.multi.Location = new System.Drawing.Point(442, 205);
+            this.multi.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(98, 74);
+            this.multi.TabIndex = 7;
+            this.multi.Text = "*";
+            this.multi.UseVisualStyleBackColor = true;
+            this.multi.Click += new System.EventHandler(this.multi_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(614, 205);
+            this.div.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(98, 74);
+            this.div.TabIndex = 8;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 354);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "結果";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(333, 349);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(160, 31);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 504);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Calc";
+            this.Text = "四則演算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multi;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ CalcFormsApplication/Calc.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcFormsApplication
+{
+    public partial class Calc : Form
+    {
+
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if ( ValidateText(textBox1.Text) && ValidateText(textBox2.Text))
+            {
+                int result = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                textBox3.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("正しい値を入力してください");
+            }
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (ValidateText(textBox1.Text) && ValidateText(textBox2.Text))
+            {
+                int result = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+                textBox3.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("正しい値を入力してください");
+            }
+        }
+
+        private void multi_Click(object sender, EventArgs e)
+        {
+            if (ValidateText(textBox1.Text) && ValidateText(textBox2.Text))
+            {
+                int result = int.Parse(textBox1.Text) * int.Parse(textBox2.Text);
+                textBox3.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("正しい値を入力してください");
+            }
+
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            if (ValidateText(textBox1.Text) && ValidateText(textBox2.Text))
+            {
+                if (textBox2.Text == "0")
+                {
+                    MessageBox.Show("0での除算はできません");
+                }
+                else
+                {
+                    int result = int.Parse(textBox1.Text) * int.Parse(textBox2.Text);
+                    textBox3.Text = result.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("正しい値を入力してください");
+            }
+        }
+
+        private bool ValidateText(string s)
+        {
+            //引数cが数字かどうかを判定
+            if (!s.All( c => Char.IsDigit(c)))
+            {
+                return false;
+            }
+            if (String.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
